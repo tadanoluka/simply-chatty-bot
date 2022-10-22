0dad8a314b776c9b8210317a5c599199132ded68
tadanoluka <simplygod39@gmail.com>
2022-10-22 14:00:04 +0200

    Stage 2/5

--- SimpleBot.kt
+++ SimpleBot.kt
@@ -3,4 +3,7 @@ package bot
 fun main() {
     println("Hello! My name is Aid.")
     println("I was created in 2022.")
+    println("Please, remind me your name.")
+    val myName = readln()
+    println("What a great name you have, ${myName}!")
 }
