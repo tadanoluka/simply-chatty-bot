2dd5fd64763f0e72220f19f173a3513fa138473b
tadanoluka <simplygod39@gmail.com>
2022-10-22 14:42:38 +0200

    Stage 3/5

--- SimpleBot.kt
+++ SimpleBot.kt
@@ -4,6 +4,13 @@ fun main() {
     println("Hello! My name is Aid.")
     println("I was created in 2022.")
     println("Please, remind me your name.")
-    val myName = readln()
-    println("What a great name you have, ${myName}!")
+    val name = readln()
+    println("What a great name you have, ${name.replaceFirstChar { it.uppercase() }}!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+    val remainder3 = readln().toInt()
+    val remainder5 = readln().toInt()
+    val remainder7 = readln().toInt()
+    val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    println("Your age is $age; that's a good time to start programming!")
 }
