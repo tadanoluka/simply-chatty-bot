ad2e3d8ee8c97c30a09becb03ca32942fd62970c
tadanoluka <simplygod39@gmail.com>
2022-10-22 14:48:48 +0200

    Stage 4/5

--- SimpleBot.kt
+++ SimpleBot.kt
@@ -13,4 +13,8 @@ fun main() {
     val remainder7 = readln().toInt()
     val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
     println("Your age is $age; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+    val countTo = readln().toInt()
+    for (i in 0..countTo) println("$i!")
+    println("Completed, have a nice day!")
 }
